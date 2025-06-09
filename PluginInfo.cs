d1714fa3aedfa168a02568b1b0bcd8f11d4502ca
BP15 <lawhitenton62@gmail.com>
2025-06-09 13:02:25 -0500

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisconnectMod
+{
+    public class PluginInfo
+    {
+        public const string GUID = "com.BP15.DisconnectMod";
+        public const string Name = "Disconnect Mod";
+        public const string Version = "1.0.0";
+    }
+}
